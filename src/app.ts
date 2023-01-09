712fe26e28d1e422f7f3148b2782173a6fb09904
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2023-01-09 13:40:01 +0100

    Added check for env file

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import {routes} from "./routes";
 import * as dotenv from "dotenv";
+import fs from "fs";
 
 const express = require("express");
 
 export const app = express();
 
-const config = dotenv.config({path: '.env'});
+if (fs.existsSync('.env')) {
+    const config = dotenv.config({path: '.env'});
 
-if (config.error) {
-    throw config.error;
+    if (config.error) {
+        throw config.error;
+    }
+} else {
+    console.log("No environment file provided");
 }
 
 routes(app);
